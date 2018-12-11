87f6dd5c7d05c6ad12492d5c596660f818ecda89
David zhang <d.ydzdavid@yahoo.com>
2018-12-11 19:07:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
+var newkittens = "Ralph"
 
-// Add your functions and code here
+function destructivelyAppendKitten (kittens, newkittens){ 
+ var kitten = kittens.concat(newkittens);
+ kittens.push(newkittens)
+ return kitten
+}
